7a8fac175c0587a4d8e718f10d3a01883067c0e1
Evelyn <Evelyn@LAPTOP-M7JVE37C>
2018-03-07 12:16:48 -0700

    1.0

---
+++ src/eva1_4_arreglos_1/EVA1_4_ARREGLOS_1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_4_arreglos_1;
+
+/**
+ *
+ * @author Evelyn
+ */
+public class EVA1_4_ARREGLOS_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECLARACION DE ARREGLOS
+        int [] aiEjemplos = new int [5];//LOS ARREGLOS SON OBJETOS 
+        int [] aiArre1, aiArre2, aiArre3, aiArre4;//ESTOS 4 IDENTIFICADORES SON ARREGLOS 
+        //LOS ARREGLOS EN JAVA SON OBJETOS 
+        int aiArre5[], i;//SOLO aiArre5 ES UN ARREGLO, i ES UNA VARIABLE ENTERA 
+        //DEFINIMOS EL TAMAÑO DEL ARREGLO CON EL OPERADOR new
+        aiArre1 = new int[100];//ASIGNAMOS 100 ESPACIOS DE TIPO ENTERO
+        //-------
+        System.out.println(aiEjemplos);//ES UNA REFERENCIA
+        System.out.println(aiEjemplos[0]);//
+    }
+    
+}
